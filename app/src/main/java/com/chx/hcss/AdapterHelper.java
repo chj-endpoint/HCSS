1e89272b4dcfd12fee4e44dc0b3e29a8e376c31b
chj-endpoint <chj5443@qq.com>
2018-03-06 22:21:42 +0800

    添加查询列表

---
+++ app/src/main/java/com/chx/hcss/AdapterHelper.java
@@ -0,0 +1,57 @@
+package com.chx.hcss;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Admin on 2018/3/6.
+ */
+
+public class AdapterHelper extends RecyclerView.Adapter<AdapterHelper.ViewHolder> {
+
+    private ArrayList<String> mData;
+
+    public AdapterHelper(ArrayList<String> data) {
+        this.mData = data;
+    }
+
+    public void updateData(ArrayList<String> data) {
+        this.mData = data;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // 实例化展示的view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        // 实例化viewholder
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // 绑定数据
+        holder.mItemView.setText(mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData == null ? 0 : mData.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mItemView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mItemView = (TextView) itemView.findViewById(R.id.list_elder);
+        }
+    }
+}
--- app/src/main/java/com/chx/hcss/User.java
+++ app/src/main/java/com/chx/hcss/User.java
@@ -3,24 +3,47 @@ package com.chx.hcss;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class User extends AppCompatActivity {
     private Button mReturnButton;
+    private RecyclerView mListRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user);
         mReturnButton = (Button)findViewById(R.id.returnback);
-
     }
     public void back_to_login(View view) {
         //setContentView(R.layout.login);
         Intent intent3 = new Intent(User.this,Login.class) ;
         startActivity(intent3);
         finish();
+    }
 
+    private void initData() {
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerAdapter = new AdapterHelper(queryData());
+    }
+
+    private void initView() {
+        mListRecyclerView = (RecyclerView) findViewById(R.id.list_view);
+        // 设置布局管理器
+        mListRecyclerView.setLayoutManager(mLayoutManager);
+        // 设置adapter
+        mListRecyclerView.setAdapter(mRecyclerAdapter);
+    }
+    private ArrayList<String> queryData(){
+
+        return null;
     }
 }
