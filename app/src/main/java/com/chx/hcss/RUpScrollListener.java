54ca9b247b4220d3dc43610f824b20f6b1367933
chj-endpoint <chj5443@qq.com>
2018-03-08 18:03:32 +0800

    上拉加载更多

---
+++ app/src/main/java/com/chx/hcss/RUpScrollListener.java
@@ -0,0 +1,14 @@
+package com.chx.hcss;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by win7 on 2018/3/8.
+ */
+
+public abstract class RUpScrollListener extends RecyclerView.OnScrollListener {
+    //RecyclerView的LayoutManager
+    private LinearLayoutManager mLLManager;
+    //
+}
