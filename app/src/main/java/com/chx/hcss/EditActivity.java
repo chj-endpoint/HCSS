53514f3de0f5d500bfc6e98f9913ad7ff445f001
chj-endpoint <chj5443@qq.com>
2018-04-20 15:04:21 +0800

    双向绑定

--- app/src/main/java/com/chx/hcss/EditActivity.java
+++ app/src/main/java/com/chx/hcss/EditActivity.java
@@ -1,20 +1,41 @@
 package com.chx.hcss;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.chx.hcss.databinding.ActivityEditBinding;
 
 public class EditActivity extends AppCompatActivity {
 
+    private ActivityEditBinding editBinding;
+    private ElderInfo elder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        editBinding = DataBindingUtil.setContentView(this, R.layout.activity_edit);
 
         Intent intentEdit = getIntent();
         long id = intentEdit.getLongExtra("id", 0);
+
+        elder = new ElderInfo();
+        elder.setId(110);
+        elder.setAddress("XiaMen University");
+        elder.setName("俞飞鸿");
+        elder.setSmok(true);
+        editBinding.setElder(elder);
+
+
 //        TextView tv = findViewById(R.id.title);
 //        tv.setText(Long.toString(id));
+    }
+
+    //查询按钮点击事件
+    public void queryClick(View view) {
+        String address = elder.getAddress();
     }
 }
--- app/src/main/java/com/chx/hcss/ElderInfo.java
+++ app/src/main/java/com/chx/hcss/ElderInfo.java
@@ -1,62 +1,120 @@
 package com.chx.hcss;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 /**
  * Created by Admin on 2018/3/19.
  */
 
-public class ElderInfo {
+public class ElderInfo extends BaseObservable {
     private long id;                //姓名
     private String name;                //姓名
     private int age;                   //年龄
     private String gender;             //性别
     private String address;            //住址
     private String extInfo;            //其他信息
+    private Boolean checkedMli;           //是否抽烟
+    private Boolean isSmok;           //是否抽烟
+    private boolean isMale;           //是否抽烟
+    private boolean isFemale;           //是否抽烟
 
+    @Bindable
     public int getAge() {
         return age;
     }
 
     public void setAge(int age) {
         this.age = age;
+        notifyPropertyChanged(BR.age);
     }
 
+    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
 
+    @Bindable
     public String getGender() {
         return gender;
     }
 
     public void setGender(String gender) {
         this.gender = gender;
+        notifyPropertyChanged(BR.gender);
     }
 
+    @Bindable
     public String getAddress() {
         return address;
     }
 
     public void setAddress(String address) {
         this.address = address;
+        notifyPropertyChanged(BR.address);
     }
 
+    @Bindable
     public String getExtInfo() {
         return extInfo;
     }
 
     public void setExtInfo(String extInfo) {
         this.extInfo = extInfo;
+        notifyPropertyChanged(BR.extInfo);
     }
 
+    @Bindable
     public long getId() {
         return id;
     }
 
     public void setId(long id) {
         this.id = id;
+        notifyPropertyChanged(BR.id);
+    }
+    @Bindable
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean male) {
+        isMale = male;
+        notifyPropertyChanged(BR.male);
+    }
+
+    @Bindable
+    public boolean isFemale() {
+        return isFemale;
+    }
+
+    public void setFemale(boolean female) {
+        isFemale = female;
+        notifyPropertyChanged(BR.female);
+    }
+
+    @Bindable
+    public Boolean getCheckedMli() {
+        return checkedMli;
+    }
+
+    public void setCheckedMli(Boolean checkedMli) {
+        this.checkedMli = checkedMli;
+        notifyPropertyChanged(BR.checkedMli);
+    }
+
+    @Bindable
+    public Boolean getSmok() {
+        return isSmok;
+    }
+
+    public void setSmok(Boolean smok) {
+        isSmok = smok;
+        notifyPropertyChanged(BR.smok);
     }
 }
